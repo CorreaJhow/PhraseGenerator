a9aaf8172abaaa8dd43ec4ccd539c4392a56f9ce
Jhow <jhonatasrcorrea@gmail.com>
2025-04-28 21:48:02 -0300

    creating controller base to start implementation

---
+++ Controllers/DemotivationalPhraseGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PhraseGenerator.Controllers
+{
+    [ApiController]
+    [Route("/v1/")]
+    public class DemotivationalPhraseGenerator
+    {
+        public DemotivationalPhraseGenerator() { }
+
+        [HttpGet("demotivationalPhrase")]
+        public string GetPhrase()
+        {
+            return "esta funcionando";
+        }
+    }
+}
